713e50a697902066d0e6471a55758bdb7e1ec762
valeponchi <81304855+valeponchi@users.noreply.github.com>
2021-07-22 19:51:44 +0100

    added .gitignore and fixed the breweriesRouter path

--- server.js
+++ server.js
@@ -1,29 +1,25 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express')
+const morgan = require('morgan')
 
 // this is the server
-const app = express();
+const app = express()
 
-// Resources 
-const breweriesRouter = require("./resources/breweries/routes")
+// Resources
+const breweriesRouter = require('./src/resources/breweries/routes')
 
 // Middlewares
-app.use(morgan("dev"));
+app.use(morgan('dev'))
 app.use(express.json()) //parse JSON body from the requests
 
 // Breweries Routes
 app.use('/breweries', breweriesRouter)
 
-app.get("*", (req, res) => { 
-  res.status(404).json({ msg: "Can't find that!"})
+app.get('*', (req, res) => {
+	res.status(404).json({ msg: "Can't find that!" })
 })
-
-
-
-
 
 // Listen for incoming requests
 
 app.listen(4000, () => {
-  console.log("I'm running!!");
-});
+	console.log("I'm running!!")
+})
